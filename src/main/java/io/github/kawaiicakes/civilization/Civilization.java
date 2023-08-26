f8aeb90c72c8bad844975c9a1d70eac042f164d3
kawaiicakes <vomitmopper@gmail.com>
2023-08-26 05:58:15 -0600

    Initial commit

---
+++ src/main/java/io/github/kawaiicakes/civilization/Civilization.java
@@ -0,0 +1,20 @@
+package io.github.kawaiicakes.civilization;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Civilization.MODID)
+public class Civilization
+{
+    public static final String MODID = "civilization";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public Civilization()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
---
+++ src/main/java/io/github/kawaiicakes/civilization/api/level/ChunkCoordinates.java
@@ -0,0 +1,4 @@
+package io.github.kawaiicakes.civilization.api.level;
+
+public class ChunkCoordinates {
+}
