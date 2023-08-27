e125ff2d582772665fc74f3c575d0732b02f5f9b
kawaiicakes <vomitmopper@gmail.com>
2023-08-27 11:34:49 -0600

    Fixing incorrect math. Continuing. Still WIP.

--- src/main/java/io/github/kawaiicakes/civilization/api/level/HexTilePos.java
+++ src/main/java/io/github/kawaiicakes/civilization/api/level/HexTilePos.java
@@ -90,33 +90,83 @@ public class HexTilePos {
     }
 
     /**
-     *
-     * @param chunkPos
-     * @return
+     * This method gets the <code>HexTilePos</code> containing the given <code>ChunkPos</code>.
+     * It relies on calculating patterns corresponding to geometric properties of a hex grid in Minecraft.
+     * Bitwise operations are used frequently as they are both performant and perfect for this job.
+     * @param chunkPos the <code>ChunkPos</code> inside of the tile to be given.
+     * @return the <code>HexTilePos</code> at the given <code>ChunkPos</code>.
      */
-    public static HexTilePos chunkToHexPos(ChunkPos chunkPos) { // TODO: local variables for bitwise operations?
-        // TODO: extract magic numbers to either local variables or static fields
+    public static HexTilePos chunkToHexPos(ChunkPos chunkPos) {
         // true only if at this z the tile is in array zero regardless of x.
         if (zInArrayZero(chunkPos.z)) {
-            return new HexTilePos(true, (chunkPos.x + 2) >> 2, -(((chunkPos.z + 2) >> 1) / 3));
+            return new HexTilePos(true, zIndependentToTileRow(true, chunkPos.z), xIndependentToTileColumn(true, chunkPos.x));
         } else if (zInArrayOne(chunkPos.z)) {
-            return new HexTilePos(false, chunkPos.x >> 2, -(((chunkPos.z - 1) >> 1) / 3));
+            return new HexTilePos(false, zIndependentToTileRow(false, chunkPos.z), xIndependentToTileColumn(false, chunkPos.x));
         } else {
             // testAgainstPattern2Skip2 is only meaningful in this context; when it is uncertain if regardless of x, the z
             // implies that we are in either array zero or one with certainty.
-            return testAgainstPattern2Skip2(chunkPos.x) ? new HexTilePos(true, (chunkPos.x + 2) >> 2, -(((chunkPos.z + 2) >> 1) / 3))
-                    : new HexTilePos(false, chunkPos.x >> 2, -(((chunkPos.z - 1) >> 1) / 3));
+            return testAgainstPattern2Skip2(chunkPos.x)
+                    ? new HexTilePos(true, zDependentToTileRow(true, chunkPos.x), xDependentToTileColumn(true, chunkPos.x))
+                    : new HexTilePos(false, zDependentToTileRow(false, chunkPos.x), xDependentToTileColumn(false, chunkPos.x));
         }
     }
 
-    /** FIXME
-     * Tests the row corresponding to a given x-coord
-     * @param arrayZero is the coord in array one or zero?
-     * @param xCoord the x coordinate in the level.
+    /*
+        HELPER METHODS BELOW -
+            Magic numbers are derived from the global origins of tiles (0, 0, 0) and (1, 0, 0).
+     */
+
+    /**
+     * Returns the row number given an x-coordinate of a <code>ChunkPos</code>. The result is meaningless unless
+     * the z-coordinate cannot be tested with certainty as being in either array zero or one regardless of x.
+     * @param arrayZero coord in array zero?
+     * @param xCoord the x coordinate corresponding to a <code>ChunkPos</code>.
      * @return  The int corresponding to the tile row.
      */
-    public static int xCoordToTileRow(boolean arrayZero, int xCoord) {
-        return arrayZero ? xCoord >> 2 : (xCoord + 2) >> 2;
+    private static int xDependentToTileColumn(boolean arrayZero, int xCoord) {
+        return arrayZero ? (xCoord + 1) >> 1 : (xCoord - 2) >> 1;
+    }
+
+    /**
+     * Returns the row number corresponding to the x-coordinate of a <code>ChunkPos</code>
+     * if and only if at any given z-coordinate, one is certain of being in array one or zero regardless of x.
+     * @param arrayZero the array of the tile is zero?
+     * @param xCoord    int representing the x-coordinate of a <code>ChunkPos</code>.
+     * @return  the tile row as int.
+     */
+    private static int xIndependentToTileColumn(boolean arrayZero, int xCoord) {
+        return arrayZero ? (xCoord + 2) >> 2 : (xCoord - 1) >> 2;
+    }
+
+    /**
+     * Returns the row number corresponding to the z-coordinate of a <code>ChunkPos</code> if and only if
+     * the tile array is uncertain for any given x at this z.
+     * <br><br>
+     * Again written by Erik of the Create: Aeronautics team.
+     * @param arrayZero is the array zero at this x and z?
+     * @param zCoord the z-coord as an int.
+     * @return the int corresponding to the tile row.
+     */
+    private static int zDependentToTileRow(boolean arrayZero, int zCoord) {
+        if (arrayZero) {
+            int a = (((zCoord + 3) & (0x80000000)) >> 31);
+            return ((zCoord + 3) - a) / 6 + a;
+        } else {
+            int a = ((zCoord & (0x80000000)) >> 31);
+            return (zCoord - a) / 6 + a;
+        }
+    }
+
+    /**
+     * Returns the row number given a z-coordinate of a <code>ChunkPos</code> if and only if
+     * the tile array is certain for any given x at this z. The sum of zCoord and 1 should
+     * never be a number that falls outside the pattern 2Skip4.
+     * @param arrayZero coord in array zero?
+     * @param zCoord the z coordinate corresponding to a <code>ChunkPos</code>.
+     * @return  The int corresponding to the tile row.
+     */
+    private static int zIndependentToTileRow(boolean arrayZero, int zCoord) {
+        return arrayZero ? (((zCoord + 1) >> 1) / 3) : (((zCoord - 2) >> 1) / 3);
     }
 
     /**
@@ -163,4 +213,24 @@ public class HexTilePos {
     private static boolean testAgainstPattern2Skip4(int n) {
         return (((n + 1) >> 1) % 3) == 0;
     }
+
+    /*  8-BIT BINARY REFERENCE SHEET
+        0  -> 00000000 > 00000000 > 00000000
+        1  -> 00000001 > 00000000 > 00000000
+        2  -> 00000010 > 00000001 > 00000000
+        3  -> 00000011 > 00000001 > 00000000
+        4  -> 00000100 > 00000010 > 00000001
+        5  -> 00000101 > 00000010 > 00000001
+        6  -> 00000110 > 00000011 > 00000001
+        7  -> 00000111 > 00000011 > 00000001
+        8  -> 00001000 > 00000100 > 00000010
+        9  -> 00001001 > 00000101 > 00000010
+        10 -> 00001010 > 00000110 > 00000010
+        11 -> 00001011 > 00000111 > 00000010
+        12 -> 00001100 > 00001000 > 00000011
+        13 -> 00001101 >
+        14 -> 00001110 >
+        15 -> 00001111 >
+        16 -> 00010000 >
+     */
 }
