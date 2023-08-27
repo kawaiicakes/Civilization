8cf40a96af2a53317a567f9e3835a8a2c601f694
kawaiicakes <vomitmopper@gmail.com>
2023-08-26 18:22:30 -0600

    Methods using bitwise logic to calculate an HECS position only from a BlockPos.

--- src/main/java/io/github/kawaiicakes/civilization/api/level/HexTilePos.java
+++ src/main/java/io/github/kawaiicakes/civilization/api/level/HexTilePos.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Class concerned with translating Hexagonal Efficient Coordinate System coordinates to & from
+ * Class concerned with translating Hexagonal Efficient Coordinate System (HECS) coordinates to & from
  * Minecraft pos values. A hex tile represents an area of 12 chunks which is owned by the nation
  * that has claimed it.
  * <br><br>
@@ -17,7 +17,8 @@ import java.util.Set;
  */
 public class HexTilePos {
     /**
-     * Tile ZERO is the tile whose global origin is equal with its local; that is, tile (0, 0, 0).
+     * Tile ZERO is the tile whose global origin is equal with its local; that is, tile (0, 0, 0). If you are unfamiliar
+     * with HECS, it is recommended you read up on it so this class makes sense and tile notation makes sense.
      */
     public static final HexTilePos ZERO = new HexTilePos(true, 0, 0);
     private boolean arrayZero;
@@ -36,6 +37,11 @@ public class HexTilePos {
      */
     public Set<ChunkPos> toChunkPositions() {
         Set<ChunkPos> toReturn = new HashSet<>();
+
+        toReturn.add(hexToGlobalOriginChunkPos(this));
+        // TODO: continue
+
+        return toReturn;
     }
 
     /**
@@ -50,7 +56,7 @@ public class HexTilePos {
      * @param hexTilePos    the <code>HexTilePos</code> you are calculating for
      * @return  the global origin of the hex tile in terms of a <code>ChunkPos</code>.
      */
-    public static ChunkPos hexToChunkPos(HexTilePos hexTilePos) {
+    public static ChunkPos hexToGlobalOriginChunkPos(HexTilePos hexTilePos) {
         return hexTilePos.arrayZero
                 ? new ChunkPos(4 * hexTilePos.row, 6 * hexTilePos.col)
                 : new ChunkPos((4 * hexTilePos.row) + 2, (6 * hexTilePos.col) + 3);
@@ -65,50 +71,69 @@ public class HexTilePos {
         ChunkPos chunk = new ChunkPos(blockPos.getX(), blockPos.getZ());
         return HexTilePos.chunkToHexPos(chunk);
     }
-    public static HexTilePos chunkToHexPos(ChunkPos chunkPos) {
 
+    /**
+     *
+     * @param chunkPos
+     * @return
+     */
+    public static HexTilePos chunkToHexPos(ChunkPos chunkPos) { // TODO: local variables for bitwise operations?
+        // true only if at this z the tile is in array zero regardless of x.
+        if (zInArrayZero(chunkPos.z)) {
+            return new HexTilePos(true, (chunkPos.x + 2) >> 2, (((chunkPos.z + 2) >> 1) / 3));
+        } else if (zInArrayOne(chunkPos.z)) {
+            return new HexTilePos(false, chunkPos.x >> 2, (((chunkPos.z - 1) >> 1) / 3));
+        } else {
+            return xInArrayZero(chunkPos.x) ? new HexTilePos(true, (chunkPos.x + 2) >> 2, (((chunkPos.z + 2) >> 1) / 3))
+                    : new HexTilePos(false, chunkPos.x >> 2, (((chunkPos.z - 1) >> 1) / 3));
+        }
     }
 
     /**
      * As 010 must appear twice every two base-10 numbers, we can leverage this fact to
-     * create a pattern, then test whether a <code>ChunkPos</code>> is in array one or zero.
+     * create a pattern, then test whether a <code>ChunkPos</code> is in array one or zero.
      * <br><br>
-     * This method is meaningless unless used within the correct context, so it has been made
-     * private.
+     * This method is only meaningful if it is uncertain whether the tile is in array zero or one
+     * at a given z-coordinate using existing methods.
      * @param xCoord an int representing the x-coordinate of a <code>ChunkPos</code>.
-     * @return  <code>true</code>
+     * @return  <code>true</code> if in array zero, <code>false</code> if in array one.
      */
-    private static boolean isInArrayZero(int xCoord) {
-        return ((xCoord+1)&2)==2;
+    private static boolean xInArrayZero(int xCoord) {
+        return ((xCoord + 1) & 2) != 2;
     }
 
-    /* TODO
-    private static boolean inTwoByFourPattern(int n) {
-        if (((n + 7) & 8) == 8) {
-            if (((n + 7) & 10) != 10)
-        }
-    }
+    /**
+     * This method, for any integer zCoord who represents the z-value of a <code>ChunkPos</code>, deduces whether zCoord in the
+     * tile (a, zCoord, c) is in array zero or array one regardless of the value of c. An elegant solution graciously
+     * written by Eriksonn of the Create: Aeronautics team.
+     * <br><br>
+     * A negative return does not necessarily mean the tile is not of array zero. Rather, more rigorous testing must
+     * be performed.
+     * @param zCoord any integer representing the z-value of a <code>ChunkPos</code>.
+     * @return  <code>true</code> if a = 0, <code>false</code> if a = 1 in tile (a, zCoord, c)
      */
+    private static boolean zInArrayZero(int zCoord) {
+        return ((zCoord >> 1) % 3) == 0;
+    }
 
+    /**
+     * This method is identical to <code>#zInArrayZero</code> including in its caveats; although is x-shifted.
+     *
+     * @param zCoord any integer representing the z-value of a <code>ChunkPos</code>.
+     * @return  <code>true</code> if a = 1, <code>false</code> if a = 0 in tile (a, zCoord, c)
+     */
+    private static boolean zInArrayOne(int zCoord) {
+        return (((zCoord - 3) >> 1) % 3) == 0;
+    }
     /*
-    0  -> 00000
-    1  -> 00001
-    2  -> 00010
-    3  -> 00011
-    4  -> 00100
-    5  -> 00101
-    6  -> 00110
-    7  -> 00111
-    8  -> 01000
-    9  -> 01001
-    10 -> 01010
-    11 -> 01011
-    12 -> 01100
-    13 -> 01101
-    14 -> 01110
-    15 -> 01111
-    16 -> 10000
-
-    0, 1, 6, 7
+        0  -> 00000 >> 2 = 00000 < 00000
+        1  -> 00001 >> 2 = 00000 < 00000
+        2  -> 00010 >> 2 = 00000 < 00001
+        3  -> 00011 >> 2 = 00000 < 00001
+        4  -> 00100 >> 2 = 00001 < 00010
+        5  -> 00101 >> 2 = 00001 < 00010
+        6  -> 00110 >> 2 = 00001 < 00011
+        7  -> 00111 >> 2 = 00001 < 00011
+        8  -> 01000 >> 2 = 00010 < 00100
      */
 }
