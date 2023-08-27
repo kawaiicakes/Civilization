c796528b1dd17df8eb4bc1fff4d6769f3613f1c0
kawaiicakes <vomitmopper@gmail.com>
2023-08-27 17:02:41 -0600

    Saving work

---
+++ src/main/java/io/github/kawaiicakes/civilization/api/level/ClaimManager.java
@@ -0,0 +1,8 @@
+package io.github.kawaiicakes.civilization.api.level;
+
+/**
+ * This class allows claims in the world to be created, changed, or deleted.
+ */
+public class ClaimManager {
+
+}
--- src/main/java/io/github/kawaiicakes/civilization/api/nation/NationManager.java
+++ src/main/java/io/github/kawaiicakes/civilization/api/nation/NationManager.java
@@ -3,5 +3,5 @@ package io.github.kawaiicakes.civilization.api.nation;
 /**
  * Instantiates a new nation; complete with hex tile claims, flag, religion, etc.
  */
-public class Nation {
+public class NationManager {
 }
