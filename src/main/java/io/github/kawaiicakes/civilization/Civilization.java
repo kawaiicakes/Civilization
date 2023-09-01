5d2f3a450ae05dea21eb03755b9430be15f46059
kawaiicakes <vomitmopper@gmail.com>
2023-09-01 13:37:03 -0600

    Fixed packets not being registered

--- src/main/java/io/github/kawaiicakes/civilization/Civilization.java
+++ src/main/java/io/github/kawaiicakes/civilization/Civilization.java
@@ -3,9 +3,11 @@ package io.github.kawaiicakes.civilization;
 import com.mojang.logging.LogUtils;
 import io.github.kawaiicakes.civilization.events.CapabilityEvents;
 import io.github.kawaiicakes.civilization.events.DebugEvents;
+import io.github.kawaiicakes.civilization.network.CivilizationPackets;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -25,5 +27,11 @@ public class Civilization
         listen(this);
         listen(DebugEvents.class);
         listen(CapabilityEvents.class);
+
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        CivilizationPackets.register();
     }
 }
