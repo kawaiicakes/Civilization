24092c86fee691f9d9be43afb24d4108b76bb5e9
kawaiicakes <vomitmopper@gmail.com>
2023-09-01 21:30:10 -0600

    Very crude texture background implementation

--- src/main/java/io/github/kawaiicakes/civilization/screen/MainScreen.java
+++ src/main/java/io/github/kawaiicakes/civilization/screen/MainScreen.java
@@ -1,12 +1,22 @@
 package io.github.kawaiicakes.civilization.screen;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+import static io.github.kawaiicakes.civilization.Civilization.MOD_ID;
 
 public class MainScreen extends Screen {
+    public static final ResourceLocation TEXTURE = new ResourceLocation(MOD_ID, "textures/gui/menu/main.png");
     public static final Component MAIN = Component.translatable("menu.civilization.main_menu");
+    private int leftPos;
+    private int topPos;
+    private static final int imageWidth = 1080;
+    private static final int imageHeight = 720;
 
     public MainScreen() {
         super(MAIN);
@@ -16,11 +26,14 @@ public class MainScreen extends Screen {
     @Override
     protected void init() {
         super.init();
+        this.leftPos = (this.width - imageWidth) / 2;
+        this.topPos = (this.height - imageHeight) / 2;
     }
 
     @Override
     public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
+        renderBgTexture(pPoseStack);
     }
 
     @Override
@@ -31,5 +44,12 @@ public class MainScreen extends Screen {
         } else {
             return false;
         }
+    }
+
+    private void renderBgTexture(PoseStack pPoseStack) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        blit(pPoseStack, 0, 0, 0, 0, imageWidth, imageHeight, this.width, this.height);
     }
 }
