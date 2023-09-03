6862dfa3abef0ffb3e03bbfcc517000f90c323dc
kawaiicakes <vomitmopper@gmail.com>
2023-09-02 23:36:55 -0600

    Experimental blit rendering for tabs

--- src/main/java/io/github/kawaiicakes/civilization/screen/MainScreen.java
+++ src/main/java/io/github/kawaiicakes/civilization/screen/MainScreen.java
@@ -3,8 +3,6 @@ package io.github.kawaiicakes.civilization.screen;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import io.github.kawaiicakes.civilization.Civilization;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
@@ -13,13 +11,24 @@ import net.minecraft.resources.ResourceLocation;
 import static io.github.kawaiicakes.civilization.Civilization.MOD_ID;
 
 public class MainScreen extends Screen {
-    public static final ResourceLocation TEXTURE = new ResourceLocation(MOD_ID, "textures/gui/menu/main.png");
-    public static final Component MAIN_SCREEN_NAME = Component.translatable("menu.civilization.main_menu");
-    public static final Component BUTTON_INFO = Component.translatable("menu.button.civilization.player_info");
+    private static final ResourceLocation TEXTURE = new ResourceLocation(MOD_ID, "textures/gui/menu/main.png");
+    private static final short textureWidth = 800;
+    private static final short textureHeight = 256;
+    private static final short bgWidth = 277;
+    private static final short bgHeight = 218;
+    private static final short tabWidth = 155;
+    private static final short tabHeight = 28;
+    private static final short tabUOffset = 277;
+    private static final short tabSelectedVOffset = 104;
+    private static final short tabSelectedBottomVOffset = 132;
+    private static final short tabUnselectedVOffset = 76;
+    private static final byte tabCount = 4;
+
+    private static final Component MAIN_SCREEN_NAME = Component.translatable("menu.civilization.main_menu");
+    private static final Component BUTTON_INFO = Component.translatable("menu.button.civilization.player_info");
     private int leftPos;
     private int topPos;
-    private static final int imageWidth = 208;
-    private static final int imageHeight = 166;
+    private byte activeTab = 0;
 
     public MainScreen() {
         super(MAIN_SCREEN_NAME);
@@ -29,19 +38,20 @@ public class MainScreen extends Screen {
     @Override
     protected void init() {
         super.init();
-        this.leftPos = (this.width - imageWidth) / 2;
-        this.topPos = (this.height - imageHeight) / 2;
-        this.addRenderableWidget(new Button(0, 0, 90, 20, BUTTON_INFO, pButton -> {
-            // TODO: make this send a C2S packet
-            Civilization.LOGGER.info("Click!");
-        }));
+        this.leftPos = (this.width - (bgWidth + tabWidth - 4)) / 2;
+        this.topPos = (this.height - bgHeight) / 2;
+
+        // this.addRenderableWidget(new Button(leftPos, topPos, Button.SMALL_WIDTH, Button.DEFAULT_HEIGHT, BUTTON_INFO, pButton -> // TODO: make this send a C2S packet Civilization.LOGGER.info("Click!")));
     }
 
     @Override
     public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
         this.renderBackground(pPoseStack);
         renderBgTexture(pPoseStack);
-        super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
+
+        renderTabs(pPoseStack);
+
+        super.render(pPoseStack, pMouseX, pMouseY, pPartialTick); //call last?
     }
 
     @Override
@@ -59,6 +69,26 @@ public class MainScreen extends Screen {
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, TEXTURE);
         RenderSystem.enableBlend();
-        this.blit(pPoseStack, this.leftPos, this.topPos, 0, 0, imageWidth, imageHeight);
+
+        //blit(pPoseStack, this.leftPos, this.topPos, 0, 0, bgWidth, bgHeight, 1024, 256);
+        blit(pPoseStack, this.leftPos, this.topPos, 0, 0, bgWidth, bgHeight, textureWidth, textureHeight);
+        this.renderTabs(pPoseStack);
+    }
+
+    private void renderTabs(PoseStack poseStack) {
+        for (byte i = 0; i < tabCount; i++) {
+            if (this.activeTab == 0 && i == 0) {
+                this.renderTab(poseStack, i, tabSelectedBottomVOffset);
+            } else if (this.activeTab == i) {
+                this.renderTab(poseStack, i, tabSelectedVOffset);
+            } else {
+                this.renderTab(poseStack, i, tabUnselectedVOffset);
+            }
+        }
+    }
+
+    private void renderTab(PoseStack poseStack, int tab, int vOffset) {
+        blit(poseStack, this.leftPos + bgWidth - 4, (this.topPos + bgHeight - tabHeight) - (tab * (tabHeight + 1)),
+                tabUOffset, vOffset, tabWidth, tabHeight, textureWidth, textureHeight);
     }
 }
