ffb950dea09eda72ac72f860a2c93339f3f7d043
kawaiicakes <vomitmopper@gmail.com>
2023-09-10 01:58:35 -0600

    leftPos and topPos may no longer be null. This is done to ensure that NullPointerExceptions aren't accidentally created down the line.

--- src/main/java/io/github/kawaiicakes/civilization/api/screen/BlitRenderDefinition.java
+++ src/main/java/io/github/kawaiicakes/civilization/api/screen/BlitRenderDefinition.java
@@ -5,17 +5,18 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Used for defining important coordinates and values for blit drawing.
  * Mainly a convenience record class for the above purposes.
+ * leftPos and topPos should be set to zero if they are not known at the time of instantiation.
  * automatically.
- * @param leftPos       the <code>Integer</code> in screen relative terms of the leftmost screen coordinate to render to.
- * @param topPos        the <code>Integer</code> in screen relative terms of the topmost screen coordinate to render to.
+ * @param leftPos       the int in screen relative terms of the leftmost screen coordinate to render to.
+ * @param topPos        the int in screen relative terms of the topmost screen coordinate to render to.
  * @param blitUOffset    the int in absolute terms representing the leftmost (x) u-coordinate to blit from.
  * @param blitVOffset    the int in absolute terms representing the topmost (y) v-coordinate to blit from.
  * @param blitUWidth     the int in absolute terms representing the number of pixels to blit along the (x) u-axis.
  * @param blitVHeight    the int in absolute terms representing the number of pixels to blit along the (y) v-axis.
  */
 public record BlitRenderDefinition(
-        @Nullable Integer leftPos,
-        @Nullable Integer topPos,
+        int leftPos,
+        int topPos,
         int blitUOffset,
         int blitVOffset,
         int blitUWidth,
