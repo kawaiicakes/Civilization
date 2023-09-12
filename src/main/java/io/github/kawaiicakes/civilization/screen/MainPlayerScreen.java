0f127aaf4ae1e36149e74dbff6a20e7293fbaa60
kawaiicakes <vomitmopper@gmail.com>
2023-09-12 01:32:53 -0600

    Changed tab text x location and begun work on rendering the player inside the screen.

--- src/main/java/io/github/kawaiicakes/civilization/screen/MainPlayerScreen.java
+++ src/main/java/io/github/kawaiicakes/civilization/screen/MainPlayerScreen.java
@@ -1,14 +1,20 @@
 package io.github.kawaiicakes.civilization.screen;
 
+import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
 import io.github.kawaiicakes.civilization.api.screen.BlitRenderDefinition;
 import io.github.kawaiicakes.civilization.api.screen.DynamicButton;
 import io.github.kawaiicakes.civilization.api.screen.SimpleGUI;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -33,6 +39,9 @@ public class MainPlayerScreen extends SimpleGUI {
     protected String activeTab;
 
     protected int tabLeftPos;
+
+    protected int xMouse;
+    protected int yMouse;
 
     public MainPlayerScreen() {
         // This call to the super constructor essentially establishes the texture location and the blitting info
@@ -80,12 +89,69 @@ public class MainPlayerScreen extends SimpleGUI {
                         + ((TAB_UNSELECTED.blitUWidth() - LOGO.blitUWidth() - 4) / 2),
                 this.topPos + 20)
         );
+
+        renderEntityInInventory(this.leftPos + 51, this.topPos + 75, 30, (float)(this.leftPos + 51) - this.xMouse, (float)(this.topPos + 75 - 50) - this.yMouse, this.minecraft.player);
     }
 
+    // Place black box at 14 25 relative to 0 0 of background
     @Override
     public void render(@NotNull PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
         this.renderTabs();
+        this.xMouse = pMouseX;
+        this.yMouse = pMouseY;
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
+    }
+
+    public static void renderEntityInInventory(int pPosX, int pPosY, int pScale, float pMouseX, float pMouseY, LivingEntity pLivingEntity) {
+        float f = (float)Math.atan((double)(pMouseX / 40.0F));
+        float f1 = (float)Math.atan((double)(pMouseY / 40.0F));
+        renderEntityInInventoryRaw(pPosX, pPosY, pScale, f, f1, pLivingEntity);
+    }
+
+    public static void renderEntityInInventoryRaw(int pPosX, int pPosY, int pScale, float angleXComponent, float angleYComponent, LivingEntity pLivingEntity) {
+        float f = angleXComponent;
+        float f1 = angleYComponent;
+        PoseStack posestack = RenderSystem.getModelViewStack();
+        posestack.pushPose();
+        posestack.translate((double)pPosX, (double)pPosY, 1050.0D);
+        posestack.scale(1.0F, 1.0F, -1.0F);
+        RenderSystem.applyModelViewMatrix();
+        PoseStack posestack1 = new PoseStack();
+        posestack1.translate(0.0D, 0.0D, 1000.0D);
+        posestack1.scale((float)pScale, (float)pScale, (float)pScale);
+        Quaternion quaternion = Vector3f.ZP.rotationDegrees(180.0F);
+        Quaternion quaternion1 = Vector3f.XP.rotationDegrees(f1 * 20.0F);
+        quaternion.mul(quaternion1);
+        posestack1.mulPose(quaternion);
+        float f2 = pLivingEntity.yBodyRot;
+        float f3 = pLivingEntity.getYRot();
+        float f4 = pLivingEntity.getXRot();
+        float f5 = pLivingEntity.yHeadRotO;
+        float f6 = pLivingEntity.yHeadRot;
+        pLivingEntity.yBodyRot = 180.0F + f * 20.0F;
+        pLivingEntity.setYRot(180.0F + f * 40.0F);
+        pLivingEntity.setXRot(-f1 * 20.0F);
+        pLivingEntity.yHeadRot = pLivingEntity.getYRot();
+        pLivingEntity.yHeadRotO = pLivingEntity.getYRot();
+        Lighting.setupForEntityInInventory();
+        EntityRenderDispatcher entityrenderdispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
+        quaternion1.conj();
+        entityrenderdispatcher.overrideCameraOrientation(quaternion1);
+        entityrenderdispatcher.setRenderShadow(false);
+        MultiBufferSource.BufferSource multibuffersource$buffersource = Minecraft.getInstance().renderBuffers().bufferSource();
+        RenderSystem.runAsFancy(() -> {
+            entityrenderdispatcher.render(pLivingEntity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, posestack1, multibuffersource$buffersource, 15728880);
+        });
+        multibuffersource$buffersource.endBatch();
+        entityrenderdispatcher.setRenderShadow(true);
+        pLivingEntity.yBodyRot = f2;
+        pLivingEntity.setYRot(f3);
+        pLivingEntity.setXRot(f4);
+        pLivingEntity.yHeadRotO = f5;
+        pLivingEntity.yHeadRot = f6;
+        posestack.popPose();
+        RenderSystem.applyModelViewMatrix();
+        Lighting.setupFor3DItems();
     }
 
     /**
@@ -164,7 +230,7 @@ public class MainPlayerScreen extends SimpleGUI {
                         poseStack.translate(0, 0, blitOffset);
                         super.renderButton(poseStack, xMouse, yMouse, tickDelta);
                         drawString(poseStack, pFont, Component.translatable("menu.civilization." + tabName),
-                                this.x + (this.renderDefinition.blitUWidth() / 2) - 6, this.y + (this.renderDefinition.blitVHeight() / 2) - 5, Integer.parseInt(hexColor, 16));
+                                this.x + 10, this.y + (this.renderDefinition.blitVHeight() / 2) - 5, Integer.parseInt(hexColor, 16));
                         poseStack.popPose();
                     }
                 }
