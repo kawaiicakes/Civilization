c0d5e34de1e015dd8d5ebaf289f7f16d3eed4125
kawaiicakes <vomitmopper@gmail.com>
2023-09-12 23:08:31 -0600

    Moved to api package as the role of NationManager is now redefined; it will expose player/civ info.

---
+++ src/main/java/io/github/kawaiicakes/civilization/api/nations/NationManager.java
@@ -0,0 +1,10 @@
+package io.github.kawaiicakes.civilization.api.nations;
+
+/**
+ * Central hub for easily accessing nation info.
+ */
+public class NationManager {
+    public static void createNation() {
+
+    }
+}
