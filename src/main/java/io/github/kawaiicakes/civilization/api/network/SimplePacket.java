7430b87cc50c437ef8f2d979977ffdd855dcc8f6
kawaiicakes <vomitmopper@gmail.com>
2023-09-13 22:32:22 -0600

    Added more JavaDoc

--- src/main/java/io/github/kawaiicakes/civilization/api/network/SimplePacket.java
+++ src/main/java/io/github/kawaiicakes/civilization/api/network/SimplePacket.java
@@ -17,7 +17,18 @@ import java.util.function.Supplier;
  */
 public abstract class SimplePacket {
     /**
-     * The constructor functions as the decoder; it is called when the recipient receives the
+     * This constructor is called when one wants to send the packet. When instantiating a new packet
+     * on query for dispatch with <code>SimpleChannel</code>, this packet is encoded with
+     * <code>#toBytes</code>, sent to the recipient, then decoded using the constructor accepting
+     * a <code>FriendlyByteBuf</code> as its argument. This creates an instance of this object on
+     * the recipient's end, which is then executed on its main thread with <code>#handle</code>.
+     */
+    public SimplePacket() {
+
+    }
+
+    /**
+     * This constructor functions as the decoder; it is called when the recipient receives the
      * <code>FriendlyByteBuf</code> from the network. It translates the <code>FriendlyByteBuf</code>
      * to a new instance of this class; where it is then consumed on the main thread of the receiving
      * side.
@@ -47,6 +58,7 @@ public abstract class SimplePacket {
     /**
      * The code inside this method will be executed on the side it is received. Use
      * <code>context</code> to access stuff like the level on the executing side.
+     * Be aware of the side you are working on at all times.
      */
     public abstract void onReceipt(NetworkEvent.Context context);
 
