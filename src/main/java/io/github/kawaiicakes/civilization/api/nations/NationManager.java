2fd262161c140761b0f8900019d33ebce8186984
kawaiicakes <vomitmopper@gmail.com>
2023-09-15 19:44:45 -0600

    new #getOverworld returns ServerLevel corresponding to the overworld. Only call when the level is not accessible by other means.

--- src/main/java/io/github/kawaiicakes/civilization/api/nations/NationManager.java
+++ src/main/java/io/github/kawaiicakes/civilization/api/nations/NationManager.java
@@ -2,7 +2,9 @@ package io.github.kawaiicakes.civilization.api.nations;
 
 import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,7 +17,7 @@ import static io.github.kawaiicakes.civilization.capabilities.CivLevelCapability
  * Central hub for easily accessing nation info. Also exists as a security checkpoint for packets received from clients.
  */
 public class NationManager {
-    public static Nation NULLARIA = new Nation(
+    public static LevelNation NULLARIA = new LevelNation(
             UUID.randomUUID(),
             "Nullaria",
             NonNullList.create(),
@@ -24,9 +26,25 @@ public class NationManager {
             0
     );
 
-    public static List<Nation> getNations(ServerLevel level) {
+    /**
+     * Only use this when absolutely necessary and ensure that it is called on the server thread.
+     * Do not cache the return as it may cause memory leaks.
+     * @return <code>null</code> if not available. Otherwise, the <code>ServerLevel</code>
+     * corresponding to the Overworld on the server.
+     */
+    public static ServerLevel getOverworld() {
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        if (server != null) {
+            if (server.getLevel(ServerLevel.OVERWORLD) != null) {
+                return server.getLevel(ServerLevel.OVERWORLD);
+            }
+        }
+        return null;
+    }
+
+    public static List<LevelNation> getNations(ServerLevel level) {
         if (!level.isClientSide()) {
-            AtomicReference<List<Nation>> listAtomicReference = new AtomicReference<>();
+            AtomicReference<List<LevelNation>> listAtomicReference = new AtomicReference<>();
             level.getCapability(CIV_LEVEL_CAP).ifPresent(levelCap -> listAtomicReference.set(levelCap.getNations()));
             return listAtomicReference.get();
         } else {
@@ -38,22 +56,22 @@ public class NationManager {
      * This should only ever be called from the server's end.
      * @param level the <code>ServerLevel</code> this is occurring on. You must ensure that the argument is passed from
      *              exclusively the server.
-     * @param nation    the <code>Nation</code> instance to query for creation. Since this argument may be passed from
+     * @param levelNation    the <code>LevelNation</code> instance to query for creation. Since this argument may be passed from
      *                  the client, a security check is done.
-     * @return      the result of attempting to create a nation. <code>true</code> indicates success,
+     * @return      the result of attempting to create a levelNation. <code>true</code> indicates success,
      */
-    public static boolean createNation(ServerLevel level, Nation nation) {
+    public static boolean createNation(ServerLevel level, LevelNation levelNation) {
         if (!level.isClientSide()) {
-            if (nationCreationValid(level, nation)) {
+            if (nationCreationValid(level, levelNation)) {
                 level.getCapability(CIV_LEVEL_CAP).ifPresent(civ -> {
                     level.players().forEach(serverPlayer -> serverPlayer.sendSystemMessage(
                             Component.literal("Successfully passed validity test!")));
-                    civ.addNation(nation);
+                    civ.addNation(levelNation);
                 });
                 return true;
             } else {
                 level.players().forEach(serverPlayer -> serverPlayer.sendSystemMessage(
-                        Component.literal("Invalid nation!")));
+                        Component.literal("Invalid levelNation!")));
                 return false;
             }
         } else {
@@ -62,29 +80,29 @@ public class NationManager {
     }
 
     /**
-     * This method exists to check, from the server, whether a nation is valid to
+     * This method exists to check, from the server, whether a levelNation is valid to
      * create. Done for security reasons as a malicious client would otherwise be able
-     * to submit any Nation instance to the server for creation.
+     * to submit any LevelNation instance to the server for creation.
      * @param level the <code>ServerLevel</code> to check validity against.
-     * @param nation    the <code>Nation</code> to check.
-     * @return  <code>true</code> if the given nation may be created. <code>false</code>
+     * @param levelNation    the <code>LevelNation</code> to check.
+     * @return  <code>true</code> if the given levelNation may be created. <code>false</code>
      *          otherwise.
      */
-    public static boolean nationCreationValid(ServerLevel level, Nation nation) {
+    public static boolean nationCreationValid(ServerLevel level, LevelNation levelNation) {
         if (level.isClientSide()) return false;
 
-        List<Nation> nationList = getNations(level);
-        if (nationList == null) return false;
-        final Stream<Nation> nationStream = nationList.stream();
+        List<LevelNation> levelNationList = getNations(level);
+        if (levelNationList == null) return false;
+        final Stream<LevelNation> nationStream = levelNationList.stream();
 
-        if (nationList.contains(nation)) return false;
+        if (levelNationList.contains(levelNation)) return false;
 
         return (
-                nationStream.noneMatch(nat -> nat.nationUUID() == nation.nationUUID())
-                && nation.territory().isEmpty()
-                && nation.players().isEmpty()
-                && nation.cities().isEmpty()
-                && nation.diplomacy() == 0
+                nationStream.noneMatch(nat -> nat.nationUUID() == levelNation.nationUUID())
+                && levelNation.territory().isEmpty()
+                && levelNation.players().isEmpty()
+                && levelNation.cities().isEmpty()
+                && levelNation.diplomacy() == 0
         );
     }
 }
