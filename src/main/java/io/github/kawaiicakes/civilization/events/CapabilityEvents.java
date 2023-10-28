17ec99f7a9437ed124dc583e4637e019159dcf80
kawaiicakes <vomitmopper@gmail.com>
2023-10-28 14:40:57 -0600

    Code optimizations. Capability now only attaches to overworld.

--- src/main/java/io/github/kawaiicakes/civilization/events/CapabilityEvents.java
+++ src/main/java/io/github/kawaiicakes/civilization/events/CapabilityEvents.java
@@ -7,16 +7,20 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.util.Objects;
+
 import static io.github.kawaiicakes.civilization.Civilization.MOD_ID;
+import static net.minecraft.world.level.Level.OVERWORLD;
 
 public class CapabilityEvents {
 
     @SubscribeEvent
-    public static void onAttachCapabilitiesLevel(AttachCapabilitiesEvent<Level> event) {
-        if (event.getObject() instanceof ServerLevel server && !event.getObject().isClientSide()) {
-            if (!server.getCapability(CivLevelCapability.Provider.CIV_LEVEL_CAP).isPresent()) {
-                event.addCapability(new ResourceLocation(MOD_ID, "level_data"), new CivLevelCapability.Provider());
-            }
-        }
+    public static void onAttachCapabilitiesOverworld(AttachCapabilitiesEvent<Level> event) {
+        if (!(event.getObject() instanceof ServerLevel level)) return;
+        if (!(Objects.equals(level.dimension(), OVERWORLD))) return;
+
+        if (level.getCapability(CivLevelCapability.Provider.CIV_LEVEL_CAP).isPresent()) return;
+
+        event.addCapability(new ResourceLocation(MOD_ID, "level_data"), new CivLevelCapability.Provider());
     }
 }
