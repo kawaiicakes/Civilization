45270d09dba9f64ef44a5dfd7f0dec8f06817677
kawaiicakes <vomitmopper@gmail.com>
2023-10-28 15:00:38 -0600

    different capability is used for each dimension.

--- src/main/java/io/github/kawaiicakes/civilization/events/CapabilityEvents.java
+++ src/main/java/io/github/kawaiicakes/civilization/events/CapabilityEvents.java
@@ -7,20 +7,16 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import java.util.Objects;
-
 import static io.github.kawaiicakes.civilization.Civilization.MOD_ID;
-import static net.minecraft.world.level.Level.OVERWORLD;
 
 public class CapabilityEvents {
 
     @SubscribeEvent
     public static void onAttachCapabilitiesOverworld(AttachCapabilitiesEvent<Level> event) {
         if (!(event.getObject() instanceof ServerLevel level)) return;
-        if (!(Objects.equals(level.dimension(), OVERWORLD))) return;
 
         if (level.getCapability(CivLevelCapability.Provider.CIV_LEVEL_CAP).isPresent()) return;
 
-        event.addCapability(new ResourceLocation(MOD_ID, "level_data"), new CivLevelCapability.Provider());
+        event.addCapability(new ResourceLocation(MOD_ID, level.dimension().location().getPath() + "_data"), new CivLevelCapability.Provider());
     }
 }
