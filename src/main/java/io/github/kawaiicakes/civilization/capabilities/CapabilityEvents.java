0d0f38ee94948f331fd9f95de64fd1373c5bf674
kawaiicakes <vomitmopper@gmail.com>
2023-10-28 20:56:02 -0600

    LevelChunk cap will store data which should be associated with a chunk

--- src/main/java/io/github/kawaiicakes/civilization/capabilities/CapabilityEvents.java
+++ src/main/java/io/github/kawaiicakes/civilization/capabilities/CapabilityEvents.java
@@ -3,11 +3,13 @@ package io.github.kawaiicakes.civilization.capabilities;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import static io.github.kawaiicakes.civilization.Civilization.MOD_ID;
 import static io.github.kawaiicakes.civilization.capabilities.CivGlobalDataCapability.Provider.CIV_GLOBAL_CAP;
+import static io.github.kawaiicakes.civilization.capabilities.CivLevelChunkCapability.Provider.CIV_LEVEL_CHUNK_CAP;
 
 public class CapabilityEvents {
 
@@ -20,5 +22,15 @@ public class CapabilityEvents {
         if (level.getCapability(CIV_GLOBAL_CAP).isPresent()) return;
 
         event.addCapability(new ResourceLocation(MOD_ID, "global_data"), new CivGlobalDataCapability.Provider());
+    }
+
+    @SubscribeEvent
+    public static void attachChunkData(AttachCapabilitiesEvent<LevelChunk> event) {
+        if (event.getObject().getLevel().isClientSide) return;
+
+        if (event.getObject().getCapability(CIV_LEVEL_CHUNK_CAP).isPresent()) return;
+
+        event.addCapability(new ResourceLocation(MOD_ID, "chunk_data"), new CivLevelChunkCapability.Provider());
+        event.getObject().setUnsaved(true);
     }
 }
