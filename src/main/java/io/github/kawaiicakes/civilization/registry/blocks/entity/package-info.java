90a3f8a82ab16125e8aac63658d1b3aaa837d09c
kawaiicakes <vomitmopper@gmail.com>
2023-11-01 11:41:53 -0600

    I HATE SQUIGGLY LINES!!!!

---
+++ src/main/java/io/github/kawaiicakes/civilization/registry/blocks/entity/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package io.github.kawaiicakes.civilization.registry.blocks.entity;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/io/github/kawaiicakes/civilization/registry/blocks/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package io.github.kawaiicakes.civilization.registry.blocks;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
